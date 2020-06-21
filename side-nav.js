73841cd62560df1a66084909351b776a5d79e60a
hdjerry <damilolajerryhd@gmail.com>
2020-06-21 14:55:46 +0100

    Favicon added

--- side-nav.js
+++ side-nav.js
@@ -1,7 +1,8 @@
 const sidenavOpenButton = document.querySelector('#top__nav__toggle');
 const sidenav = document.querySelector('#sideNav');
 const sideNavCloseButton = document.querySelector('#body__click');
-
+// console.log(sideNavCloseButton);
+//
 sidenavOpenButton.addEventListener('click', () => {
     // body.classList.add('body__active');
     sidenav.classList.add('sidenav__active');
@@ -13,4 +14,3 @@ sideNavCloseButton.addEventListener('click', () => {
     sidenav.classList.add('sidenav__unactive');
     sideNavCloseButton.classList.remove('body__click__active');
 });
-
